288bbfd00afa20ad5f3378866b080843ef1d87b6
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-07 21:26:56 +0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Giovanni Gaffé on 2021/10/7.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
