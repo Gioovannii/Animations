9d76ce73c1e5f87a61d60e88f81caecb0878e12f
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-09 11:01:57 +0800

    NEw file

---
+++ Animations/AnimationBinding.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationBinding.swift
+//  Animations
+//
+//  Created by Giovanni Gaffé on 2021/10/9.
+//
+
+import SwiftUI
+
+struct AnimationBinding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationBinding_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationBinding()
+    }
+}
