d081ae28aec764b86714d9ea6f7e7d086545aa85
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-09 11:02:19 +0800

    Update content view

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,35 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
-    
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(Color.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                    .stroke(Color.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(Double(2 - animationAmount))
-                .animation(
-                    Animation.easeInOut(duration: 1)
-        //                .delay(1)
-        //                .repeatCount(3, autoreverses: true)
-                        .repeatForever(autoreverses: false)
-                )
-            )
-        .onAppear {
-            animationAmount = 2
-        }
-//        .scaleEffect(animationAmount)
-        //        .blur(radius: (animationAmount - 1) * 3)
-        //        .animation(.interpolatingSpring(stiffness: 50, damping: 1))
-        
+        Text("Hello, World!")
     }
 }
 
