c5dc7af8c9d1eb5028f9d90216fdbdfe106c2c79
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-11 11:52:31 +0800

    Add some code to show or not views

--- Animations/ViewTransitions.swift
+++ Animations/ViewTransitions.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct ViewTransitions: View {
+    @State private var isShowingRed = false
     var body: some View {
         VStack {
             Button("Tap Me") {
                 // do nothing
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                    .transition(.scale)
+                
             }
         }
     }
