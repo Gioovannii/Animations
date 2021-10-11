95702eeee3ac3a1f183994365a682e3ea773e7eb
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-11 12:32:02 +0800

    set Custom transition using view modifier

--- Animations/ViewTransitions.swift
+++ Animations/ViewTransitions.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
 struct ViewTransitions: View {
     @State private var isShowingRed = false
     var body: some View {
         VStack {
             Button("Tap Me") {
-                // do nothing
                 withAnimation {
                     isShowingRed.toggle()
                 }
@@ -22,8 +36,8 @@ struct ViewTransitions: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
-//                    .transition(.scale)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
                 
             }
         }
