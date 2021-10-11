51ba4fdb3049aa1ae90c486073b4ffc9aa3313d9
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-11 11:45:18 +0800

    New file for transitions

---
+++ Animations/ViewTransitions.swift
@@ -0,0 +1,20 @@
+//
+//  ViewTransitions.swift
+//  Animations
+//
+//  Created by Giovanni Gaffé on 2021/10/11.
+//
+
+import SwiftUI
+
+struct ViewTransitions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewTransitions_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewTransitions()
+    }
+}
